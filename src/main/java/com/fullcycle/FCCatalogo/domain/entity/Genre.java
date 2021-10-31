0f004176bdc9c09d94cb4e8fd349009070756e44
RayssaNeder <rayssafreitas13@gmail.com>
2021-10-31 09:20:25 -0300

    Inclui Lista de categoria na classe Genre

--- src/main/java/com/fullcycle/FCCatalogo/domain/entity/Genre.java
+++ src/main/java/com/fullcycle/FCCatalogo/domain/entity/Genre.java
@@ -1,21 +1,54 @@
 package com.fullcycle.FCCatalogo.domain.entity;
 
+import java.util.List;
 import java.util.UUID;
 
 public class Genre extends BaseEntity {
 
     private String name;
+    private List<Category> categories;
 
     public Genre(){}
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        if(categories == null) throw new IllegalArgumentException("");
+        this.categories = categories;
+    }
 
     public Genre(String name){
         super.generateUUID();
         this.setName(name);
     }
 
+    public Genre(UUID id,String name, List<Category> categories){
+        super.setId(id);
+        this.setName(name);
+        this.setCategories(categories);
+    }
+
+    public Genre(String name, List<Category> categories){
+        super.generateUUID();
+        this.setName(name);
+        this.setCategories(categories);
+    }
+
     public Genre(UUID id,String name){
         super.setId(id);
         this.setName(name);
+    }
+
+    public void addCategory(Category category){
+        if(categories == null) throw new IllegalArgumentException("");
+        this.categories.add(category);
+    }
+
+    public void removeCategory(Category category){
+        if(categories == null) throw new IllegalArgumentException("");
+        this.categories.removeIf(c -> this.categories.contains(category));
     }
 
     public String getName() {
@@ -27,6 +60,8 @@ public class Genre extends BaseEntity {
         if(name.length() < 0) throw new IllegalArgumentException("");
         this.name = name;
     }
+
+    
 
    
     
